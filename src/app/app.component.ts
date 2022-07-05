ebb2ec33cfa7f2d6cf192e5362d8c0274965dad6
Wojciech Okoński <wojteko22.96@gmail.com>
2022-07-05 09:51:21 +0000

    Use lodash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-common-js';
+
+  constructor() {
+    console.warn(_.uniq([2, 1, 2]));
+  }
 }
