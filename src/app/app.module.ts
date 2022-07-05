2c31feb85e83581bfecdf8a89011134be5a49567
Wojciech Okoński <wojteko22.96@gmail.com>
2022-07-05 09:57:58 +0000

    Register locale: no more warning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
